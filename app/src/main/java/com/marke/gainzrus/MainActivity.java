d0d9c8c1ef33cfd9c2572df0075a7fab8c891de3
Steven Dew <stevendew02@gmail.com>
2023-10-05 20:07:55 -0500

    📦  FEAT: wippy commity

--- app/src/main/java/com/marke/gainzrus/MainActivity.java
+++ app/src/main/java/com/marke/gainzrus/MainActivity.java
@@ -5,6 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,5 +21,12 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().hide();
 
         setContentView(R.layout.activity_main);
+    }
+
+    // The click handler method defined in the XML layout
+    public void onButtonClick(View view) {
+        // Your code to handle the button click event
+        // For example, you can display a Toast message
+        Toast.makeText(this, "Button Clicked!", Toast.LENGTH_SHORT).show();
     }
 }
