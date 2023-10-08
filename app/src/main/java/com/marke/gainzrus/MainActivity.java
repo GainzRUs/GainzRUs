a2aee580fb3e86fc845dd5946626c104531bbb28
Steven Dew <stevendew02@gmail.com>
2023-10-08 17:10:10 -0500

    📦  FEAT: add ability to navigate to new screen

--- app/src/main/java/com/marke/gainzrus/MainActivity.java
+++ app/src/main/java/com/marke/gainzrus/MainActivity.java
@@ -3,11 +3,9 @@ package com.marke.gainzrus;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.Window;
-import android.view.WindowManager;
-import android.app.Activity;
-import android.os.Bundle;
+import android.content.Intent;
 import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
 import android.widget.Toast;
 
 
@@ -24,9 +22,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // The click handler method defined in the XML layout
-    public void onButtonClick(View view) {
-        // Your code to handle the button click event
-        // For example, you can display a Toast message
-        Toast.makeText(this, "Button Clicked!", Toast.LENGTH_SHORT).show();
+    public void onAddExerciseClick(View view) {
+        Intent intent = new Intent(MainActivity.this, AddExercise.class);
+        startActivity(intent);
     }
 }
