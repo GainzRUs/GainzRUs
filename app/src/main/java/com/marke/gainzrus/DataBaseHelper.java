0cc93f4ca0e1d11d39da02ae1484f763d815a68d
Steven Dew <stevendew02@gmail.com>
2023-11-17 20:03:19 -0600

    📦  FEAT: add query for exercises on workout history screen

--- app/src/main/java/com/marke/gainzrus/DataBaseHelper.java
+++ app/src/main/java/com/marke/gainzrus/DataBaseHelper.java
@@ -2,10 +2,18 @@ package com.marke.gainzrus;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 
 public class DataBaseHelper extends SQLiteOpenHelper {
 
@@ -18,9 +26,10 @@ public class DataBaseHelper extends SQLiteOpenHelper {
     public static final String COLUMN_EXERCISE_ID_FK = "EXERCISE_ID"; // Foreign key linking to EXERCISE_TABLE
     public static final String COLUMN_NUMBER_OF_REPS = "NUMBER_OF_REPS";
     public static final String COLUMN_WEIGHT = "WEIGHT";
+    public static final String COLUMN_DATE_CREATED = "DATE_CREATED";
 
     public DataBaseHelper(@Nullable Context context) {
-        super(context, "workout.db", null, 2);
+        super(context, "workout.db", null, 5);
     }
 
     @Override
@@ -28,7 +37,8 @@ public class DataBaseHelper extends SQLiteOpenHelper {
         // Create Exercise Table
         String createExerciseTableStatement = "CREATE TABLE " + EXERCISE_TABLE + " (" +
                 COLUMN_EXERCISE_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                COLUMN_EXERCISE_NAME + " TEXT)";
+                COLUMN_EXERCISE_NAME + " TEXT, " +
+                COLUMN_DATE_CREATED + " TEXT)"; // Add dateCreated column
         db.execSQL(createExerciseTableStatement);
 
         // Create Sets Table
@@ -58,6 +68,7 @@ public class DataBaseHelper extends SQLiteOpenHelper {
 
         ContentValues exerciseValues = new ContentValues();
         exerciseValues.put(COLUMN_EXERCISE_NAME, exercise.getExerciseName());
+        exerciseValues.put(COLUMN_DATE_CREATED, getCurrentDateTime());
         long exerciseId = db.insert(EXERCISE_TABLE, null, exerciseValues);
 
         if (exerciseId == -1) {
@@ -77,5 +88,76 @@ public class DataBaseHelper extends SQLiteOpenHelper {
         }
 
         return true; // Return true if both exercise and sets are inserted successfully
+    }
+
+
+    // In DataBaseHelper class
+    public List<Exercise> getAllExercises() {
+        List<Exercise> exercises = new ArrayList<>();
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        // Query to retrieve all exercises
+        String query = "SELECT * FROM " + EXERCISE_TABLE;
+        Cursor cursor = db.rawQuery(query, null);
+
+        // Iterate through the cursor and populate the list
+        if (cursor.moveToFirst()) {
+            do {
+                // Check if the column index is valid
+                int columnIndex = cursor.getColumnIndex(COLUMN_EXERCISE_ID);
+                int exerciseId = (columnIndex != -1) ? cursor.getInt(columnIndex) : -1;
+
+                columnIndex = cursor.getColumnIndex(COLUMN_EXERCISE_NAME);
+                String exerciseName = (columnIndex != -1) ? cursor.getString(columnIndex) : "";
+
+                columnIndex = cursor.getColumnIndex(COLUMN_DATE_CREATED);
+                String dateCreated = (columnIndex != -1) ? cursor.getString(columnIndex) : "";
+
+                // Parse the date string into a Date object
+                Date parsedDate = null;
+                if (!dateCreated.isEmpty()) {
+                    try {
+                        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault());
+                        parsedDate = dateFormat.parse(dateCreated);
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                        // Handle the exception if the date string cannot be parsed
+                    }
+                }
+
+
+                // Query to retrieve sets for the current exercise
+                String setsQuery = "SELECT * FROM " + SETS_TABLE +
+                        " WHERE " + COLUMN_EXERCISE_ID_FK + " = ?";
+                Cursor setsCursor = db.rawQuery(setsQuery, new String[]{String.valueOf(exerciseId)});
+
+                List<ExerciseSet> exerciseSets = new ArrayList<>();
+                while (setsCursor.moveToNext()) {
+                    // Check if the column index is valid for reps
+                    int repsColumnIndex = setsCursor.getColumnIndex(COLUMN_NUMBER_OF_REPS);
+                    int reps = (repsColumnIndex != -1) ? setsCursor.getInt(repsColumnIndex) : 0;
+
+                    // Check if the column index is valid for weight
+                    int weightColumnIndex = setsCursor.getColumnIndex(COLUMN_WEIGHT);
+                    double weight = (weightColumnIndex != -1) ? setsCursor.getDouble(weightColumnIndex) : 0.0;
+
+                    exerciseSets.add(new ExerciseSet(reps, weight));
+                }
+                setsCursor.close();
+
+                exercises.add(new Exercise(exerciseName,  parsedDate, exerciseSets));
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+
+        return exercises;
+    }
+
+
+
+    private String getCurrentDateTime() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault());
+        Date date = new Date();
+        return dateFormat.format(date);
     }
 }
