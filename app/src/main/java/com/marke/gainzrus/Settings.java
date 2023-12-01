869530d8c3a841e5d1f97fe5acbe779d54e7cf2b
MarkCodez <markeskander745@gmail.com>
2023-12-01 16:34:21 -0600

    mark - changes to home, stats, settings pages. still wip.

--- app/src/main/java/com/marke/gainzrus/Settings.java
+++ app/src/main/java/com/marke/gainzrus/Settings.java
@@ -4,11 +4,27 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Settings extends AppCompatActivity {
+
+    private EditText userNameText;
+    private EditText userWeightText;
+    private EditText userFeetText;
+    private EditText userInchText;
+    private TextView userBMIText;
+    private Switch musicSwitch;
+    private SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +32,83 @@ public class Settings extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
 
         getSupportActionBar().setTitle("Settings");
+
+        // Find the Spinner view by its ID
+        Spinner spinner_menu = findViewById(R.id.spinner_menu);
+
+        // Assuming these are the names of your activities or pages
+        String[] pageNames = {"Settings", "Stats activity", "Profile", "Workout History", "Add Exercise"};
+
+        // Find the Spinner view by its ID
+        spinner_menu = findViewById(R.id.spinner_menu);
+
+        // Initialize the Spinner with page names
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, pageNames);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner_menu.setAdapter(adapter);
+
+        // Set listener for Spinner item selection
+        spinner_menu.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedItem = parent.getItemAtPosition(position).toString();
+
+                // Perform actions based on the selected item
+                switch (selectedItem) {
+                    case "Profile":
+                        // Navigate to Activity related to Option 1
+                        Intent option1Intent = new Intent(Settings.this, ProfileSetup.class);
+                        startActivity(option1Intent);
+                        break;
+                    case "Workout History":
+                        // Navigate to Activity related to Option 2
+                        Intent option2Intent = new Intent(Settings.this, WorkoutHistory.class);
+                        startActivity(option2Intent);
+                        break;
+                    case "Add Exercise":
+                        // Navigate to Activity related to Option 3
+                        Intent option3Intent = new Intent(Settings.this, AddExercise.class);
+                        startActivity(option3Intent);
+                        break;
+                    case "Stats activity":
+                        // Navigate to Activity related to Option 3
+                        Intent option4Intent = new Intent(Settings.this, StatsActivity.class);
+                        startActivity(option4Intent);
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Handle scenario when nothing is selected (if needed)
+            }
+        });
+
+        musicSwitch = findViewById(R.id.musicSwitch);
+        sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+
+        boolean isMusicOn = sharedPreferences.getBoolean("musicState", true);
+        musicSwitch.setChecked(isMusicOn);
+
+        musicSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putBoolean("musicState", isChecked);
+            editor.apply();
+
+            String toastMessage = isChecked ? "Music on" : "Music off";
+            Toast.makeText(Settings.this, toastMessage, Toast.LENGTH_SHORT).show();
+        });
+
+        findViews();
+    }
+
+    // searches for/initializes values on profile screen
+    private void findViews() {
+        userNameText = findViewById(R.id.userValue);
+        userFeetText = findViewById(R.id.heightFeetValue);
+        userInchText = findViewById(R.id.heightInchValue);
+        userWeightText = findViewById(R.id.weightValue);
+        userBMIText = findViewById(R.id.bmiValue);
     }
 
     public void onClickClearData(View view) {
@@ -27,14 +120,15 @@ public class Settings extends AppCompatActivity {
             dbHelper.clearDatabase();
             Toast.makeText(this, "All data cleared", Toast.LENGTH_SHORT).show();
         });
-        alertDialogBuilder.setNegativeButton("No", (dialogInterface, i) -> {});
+        alertDialogBuilder.setNegativeButton("No", (dialogInterface, i) -> {
+        });
 
         // Create and show the AlertDialog
         AlertDialog alertDialog = alertDialogBuilder.create();
         alertDialog.show();
     }
 
-    public void onClickHomePage(View view){
+    public void onClickHomePage(View view) {
         Intent intent = new Intent(Settings.this, MainActivity.class);
         startActivity(intent);
     }
